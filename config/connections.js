865279264ae3d4d5fc0029250f679a59bcbcc8b6
Nullises <texanico@gmail.com>
2017-01-15 20:05:38 -0400

    Conexion a DB remota

--- config/connections.js
+++ config/connections.js
@@ -74,14 +74,22 @@ module.exports.connections = {
   *                                                                          *
   *                                                                          *
   ***************************************************************************/
+    //LOCAL
    postgresql: {
      adapter: 'sails-postgresql',
      host: '127.0.0.1',
      user: 'postgres',
      password: '123456uv',
      database: 'codis_portal'
+   },
+  //PRODUCTION
+   remotePostgresql: {
+     adapter: 'sails-postgresql',
+     host: 'ec2-54-225-104-61.compute-1.amazonaws.com',
+     user: 'pqtodgrscsabrd',
+     password: '9b686e6f285092ef7ea1b46f712a255a8c21efcbf3c617a2726fa000e4dc6bcc',
+     database: 'd43omdv87kqeqj'
    }
-
 
   /***************************************************************************
   *                                                                          *
